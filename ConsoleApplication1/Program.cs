84400dbdecbfd13334c73bf7153fe20cc1f9fb76
Suraj Deshpande <deshpande.suraj@outlook.com>
2014-03-17 16:23:30 +0530

    initial Commit
    C# Extension methods example

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExtensionMethodsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            int i = 41;
+
+            Console.WriteLine(str.IsNullOrEmpty());
+            Console.WriteLine(i.IsEvenNumber());
+            Console.ReadLine();
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static bool IsNullOrEmpty(this string input)
+        {
+            if (input == null || input == string.Empty)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsEvenNumber(this int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
